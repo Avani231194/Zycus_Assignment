4ba007c2e9a5c6a7b1177d6b9abbaef67a9e279d
avani231194 <avanisinghal1@gmail.com>
2017-07-17 16:18:56 +0530

    Script

--- Assignment2/script.sh
+++ Assignment2/script.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-hostname=$@
-username=vagrant
+#!/bin/bash  #Shebang 
+hostname=$@  #This will take the ip addresses as an input from the user
+username=root  #username is root, this can be anything
 
-for i in $(echo $hostname | sed "s/,/ /g"); do
-  ssh $username@$i "
-  hostname -I | cut -d ' ' -f2  ;uptime | grep -ohe '"load average[s:][: ].*"'
-  "
+for i in $(echo $hostname | sed "s/,/ /g"); do   #This will take the argument (comma seperated)
+  ssh $username@$i "hostname -I | cut -d ' ' -f2  ;uptime | grep -ohe '"load average[s:][: ].*"'"  #These are the commands that will first login to the server run the commands will display the output 
 done
